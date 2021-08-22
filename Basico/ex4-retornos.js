0eb631e9dd7cc65b2a6361e7d71e0ccc316d9f42
Araya <joaoaraya7@gmail.com>
2021-08-22 23:28:27 +0000

    funções de retornos

---
+++ Basico/ex4-retornos.js
@@ -0,0 +1,22 @@
+function somar(n1=0, n2=0)
+{
+    return `${n1} + ${n2} = ${n1+n2}`;
+}
+
+function parimp(n=0)
+{
+    if (n%2 == 0)
+    {
+        return `${n} é par!`;
+    }
+    else
+    {
+        return `${n} é ímpar!`;
+    }
+}
+
+window.alert(somar(5,20)); 
+// Chamada(alert) + ação(somar) + valor de entrada(5,20) = return da função(25)
+
+window.alert(parimp(9));
+// Chamada(alert) + ação(parimp) + valor de entrada(9) = return da função(ímpar)
