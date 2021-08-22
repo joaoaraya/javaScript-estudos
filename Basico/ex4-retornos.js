d36f484da0a048e3a855a5f211d5d5fb8856047a
Araya <joaoaraya7@gmail.com>
2021-08-22 23:30:55 +0000

    funções e retornos

--- Basico/ex4-retornos.js
+++ Basico/ex4-retornos.js
@@ -17,6 +17,7 @@ function parimp(n=0)
 
 window.alert(somar(5,20)); 
 // Chamada(alert) + ação(somar) + valor de entrada(5,20) = return da função(25)
+// O 'alert' está chamando a ação(função) 'somar' com os valores de entrada '5 e 20' para a função 'somar retornar o valor no alert (25)
 
 window.alert(parimp(9));
 // Chamada(alert) + ação(parimp) + valor de entrada(9) = return da função(ímpar)
